dac3b38b633d46096aafff22f465c4a11c51373a
ares <zhouzm@infogo.com.cn>
2023-03-28 16:27:42 +0800

    ball's reflection

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"github.com/ares0516/snake/pkg/component"
+	"github.com/ares0516/snake/pkg/define"
+	"github.com/hajimehoshi/ebiten/v2"
+)
+import "image/color"
+
+type MyGame struct {
+	screenWidth, screenHeight int
+	ball                      *component.Square
+}
+
+func NewMyGame() *MyGame {
+	return &MyGame{
+		screenWidth:  640,
+		screenHeight: 480,
+	}
+}
+
+func (g *MyGame) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return g.screenWidth, g.screenHeight
+}
+
+func (g *MyGame) Update() error {
+
+	if ebiten.IsKeyPressed(ebiten.KeySpace) {
+		g.ball.IsRun = true
+	}
+
+	if g.ball.IsRun {
+		g.ball.CollisionDetection(float64(g.screenWidth), float64(g.screenHeight))
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+
+	}
+
+	return nil
+}
+
+// 在屏幕上绘制游戏内容
+func (g *MyGame) Draw(screen *ebiten.Image) {
+	// 绘制黑色背景
+	screen.Fill(color.RGBA{0, 0, 0, 255})
+
+	// 绘制图像
+	screen.DrawImage(g.ball.Image, g.ball.Opts)
+
+}
+
+func main() {
+	game := NewMyGame()
+	//                                颜色       高度   宽度   中心点x    中心点y  步长
+	game.ball = component.NewSquare(define.Red, 5, 5, 157.5, 225, 3)
+
+	ebiten.SetWindowTitle("SNAKE")
+
+	if err := ebiten.RunGame(game); err != nil {
+		panic(err)
+	}
+}
---
+++ pkg/component/square.go
@@ -0,0 +1,74 @@
+package component
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"image/color"
+	"log"
+	"math"
+	"math/rand"
+)
+
+type Square struct {
+	bgc   color.RGBA
+	h     float64
+	w     float64
+	x     float64
+	y     float64
+	step  float64
+	angle float64
+	stepX float64
+	stepY float64
+	alive bool
+	IsRun bool
+	Image *ebiten.Image
+	Opts  *ebiten.DrawImageOptions
+}
+
+func NewSquare(bgc color.RGBA, h, w int, x, y, step float64) *Square {
+	image := ebiten.NewImage(w, h)
+	image.Fill(bgc)
+	opts := &ebiten.DrawImageOptions{}
+	opts.GeoM.Translate(x, y)
+	angle := float64(rand.Intn(120) + 30)
+	//angle := float64(0)
+	return &Square{
+		bgc:   bgc,
+		h:     float64(h),
+		w:     float64(w),
+		x:     x,
+		y:     y,
+		step:  step,
+		angle: angle,
+		stepX: step * math.Cos(angle),
+		stepY: step * math.Sin(angle),
+		alive: true,
+		Image: image,
+		Opts:  opts,
+	}
+}
+
+func (s *Square) CollisionDetection(w, h float64) {
+	x, y := s.x+s.stepX, s.y+s.stepY // 小球下一步位置坐标
+	tx, ty := s.stepX, s.stepY
+	if x <= 0 {
+		tx = -s.x
+		ty = tx * math.Tan(s.angle) // y轴镜像反射
+		s.stepX *= -1               // x轴转向
+	} else if x >= w {
+		tx = -(w - s.x)
+		ty = tx * math.Tan(s.angle)
+		s.stepX *= -1
+	} else if y <= 0 {
+		ty = -s.y
+		tx = ty / math.Tan(s.angle)
+		s.stepY *= -1
+	} else if y >= h {
+		ty = -(h - s.y)
+		tx = ty / math.Tan(s.angle)
+		s.stepY *= -1
+	}
+	log.Printf("x : %f , tx %f, y: %f, ty: %f", x, tx, y, ty)
+	s.x += tx
+	s.y += ty
+	s.Opts.GeoM.Translate(tx, ty)
+}

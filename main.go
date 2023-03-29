97a636168628c291a1e1cfebe487e38c677a1dd8
ares <zhouzm@infogo.com.cn>
2023-03-29 16:21:21 +0800

    playable

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import "image/color"
 type MyGame struct {
 	screenWidth, screenHeight int
 	ball                      *component.Square
+	board                     *component.Square
 }
 
 func NewMyGame() *MyGame {
@@ -30,14 +31,14 @@ func (g *MyGame) Update() error {
 	}
 
 	if g.ball.IsRun {
-		g.ball.CollisionDetection2(float64(g.screenWidth), float64(g.screenHeight))
+		g.ball.CollisionDetection3(float64(g.screenWidth), float64(g.screenHeight), g.board)
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-
+		g.board.Move(g.screenWidth, -5, g.ball)
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyRight) {
-
+		g.board.Move(g.screenWidth, 5, g.ball)
 	}
 
 	return nil
@@ -50,6 +51,7 @@ func (g *MyGame) Draw(screen *ebiten.Image) {
 
 	// 绘制图像
 	screen.DrawImage(g.ball.Image, g.ball.Opts)
+	screen.DrawImage(g.board.Image, g.board.Opts)
 
 }
 
@@ -57,7 +59,8 @@ func main() {
 	game := NewMyGame()
 	//                                颜色       高度   宽度   中心点x    中心点y  步长
 	//game.ball = component.NewSquare(define.Red, 240, 320, 157.5, 225, 2)
-	game.ball = component.NewSquare(define.Red, 240, 320, 0, 0, 20)
+	game.ball = component.NewSquare(define.Red, 5, 5, 300, 395, 3)
+	game.board = component.NewSquare(define.White, 5, 40, 285, 400, 0)
 
 	ebiten.SetWindowTitle("SNAKE")
 
--- pkg/component/square.go
+++ pkg/component/square.go
@@ -20,6 +20,7 @@ type Square struct {
 	stepY float64
 	alive bool
 	IsRun bool
+	score int
 	Image *ebiten.Image
 	Opts  *ebiten.DrawImageOptions
 }
@@ -29,7 +30,7 @@ func NewSquare(bgc color.RGBA, h, w int, x, y, step float64) *Square {
 	image.Fill(bgc)
 	opts := &ebiten.DrawImageOptions{}
 	opts.GeoM.Translate(x, y)
-	angle := float64(rand.Intn(120) + 30)
+	angle := float64(240+rand.Intn(60)) * math.Pi / 180
 	//angle := float64(0)
 	return &Square{
 		bgc:   bgc,
@@ -47,7 +48,40 @@ func NewSquare(bgc color.RGBA, h, w int, x, y, step float64) *Square {
 	}
 }
 
-func (s *Square) CollisionDetection2(w, h float64) {
+// game.ball = component.NewSquare(define.Red, 5, 5, 300, 390, 3)
+// game.board = component.NewSquare(define.White, 5, 40, 285, 400, 0)
+func (s *Square) CollisionDetection3(w, h float64, board *Square) {
+	x, y := s.x+s.stepX, s.y+s.stepY // 小球下一步位置坐标
+	log.Printf("current squre x[%f]y[%f]", s.x, s.y)
+	log.Printf("suqre stepX[%f]stepY[%f]", s.stepX, s.stepY)
+	log.Printf("squre next x[%f]y[%f]", x, y)
+	log.Printf("w[%f]h[%f]", w, h)
+	if x <= 0 {
+		s.stepX *= -1
+		log.Printf("window left")
+	} else if x+s.w >= w {
+		s.stepX *= -1
+		log.Printf("window right")
+	} else if y <= 0 {
+		s.stepY *= -1
+		log.Printf("window top")
+	} else if (x+s.w >= board.x) && (x <= board.x+board.w) && y+s.h >= board.y { // 小球与板子上表面发生碰撞
+		// 小球右侧坐标大于等于板子左侧坐标，小球左侧坐标小于等于板子右侧坐标，小球下侧坐标小于等于板子上侧坐标
+		s.score++
+		s.stepY *= -1
+		log.Printf("board top")
+	} else if y+s.h >= h { // 小球落地
+		s.alive = false
+		log.Printf("window bottom")
+		return
+	}
+	tx, ty := s.stepX, s.stepY
+	s.x += tx
+	s.y += ty
+	s.Opts.GeoM.Translate(tx, ty)
+}
+
+func (s *Square) CollisionDetection2(w, h float64, board *Square) {
 	x, y := s.x+s.stepX, s.y+s.stepY // 小球下一步位置坐标
 	log.Printf("x[%f]y[%f]", x, y)
 	if x <= 0 {
@@ -91,4 +125,33 @@ func (s *Square) CollisionDetection(w, h float64) {
 	s.x += tx
 	s.y += ty
 	s.Opts.GeoM.Translate(tx, ty)
+}
+
+// Move board 的移动逻辑，在未开球的时候，小球也跟着board移动
+func (s *Square) Move(w int, step float64, ball *Square) {
+	W := float64(w)
+	if s.x+step < 0 {
+		s.Opts.GeoM.Translate(0-s.x, 0)
+		if !ball.IsRun {
+			ball.Opts.GeoM.Translate(0-s.x, 0)
+			ball.x += -s.x
+		}
+		s.x = 0
+		return
+	}
+	if s.x+step+s.w > W {
+		s.Opts.GeoM.Translate(W-s.x-s.w, 0)
+		if !ball.IsRun {
+			ball.Opts.GeoM.Translate(W-s.x-s.w, 0)
+			ball.x += W - s.x - s.w
+		}
+		s.x = W - s.w
+		return
+	}
+	s.x += step
+	s.Opts.GeoM.Translate(step, 0)
+	if !ball.IsRun {
+		ball.Opts.GeoM.Translate(step, 0)
+		ball.x += step
+	}
 }
